9f1d14841be51af5e799db5b421e3fa0a856e159
b314028 <b314028@m.nagahama-i-bio.ac.jp>
2017-06-23 17:54:54 +0900

    test 7-5

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -15,6 +15,10 @@ class BuzzerTest(unittest.TestCase):
 			pub.publish(1234)
 			time.sleep(0.1)
 
+		with open("/dev/rtbuzzer0","r") as f:
+			data = f.readline()
+			self.assertEqual(data,"1234\n",!value dose not written to rtbuzzer0")
+
 if __name__ == '__main__':
 	time.sleep(3)
 	rospy.init_node('travis_test_buzzer')
