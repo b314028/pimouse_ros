9098d2a4baa777162b9ce6a35e1ea1053607991e
b314028 <b314028@m.nagahama-i-bio.ac.jp>
2017-07-10 16:06:20 +0900

    7_10 8_1

--- test/travis_test_motors2.py
+++ test/travis_test_motors2.py
@@ -57,7 +57,7 @@ class MotorTest(unittest.TestCase):
 		self.assertEqual(ret.message, "OFF", "motor off wrong message")
 		with open("/dev/rtmotoren0","r") as f:
 			data = f.readline()
-			self.assertEqual(data,"1\n","wrong value in rtmotor0 at motor on")
+			self.assertEqual(data,"0\n","wrong value in rtmotor0 at motor on")
 
 if __name__ == "__main__":
 	rospy.init_node('travis_test_motors')
